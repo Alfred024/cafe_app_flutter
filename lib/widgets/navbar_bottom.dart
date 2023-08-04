16d1f14758b50b70b703d7522609b521cca6f0b1
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-03 23:42:19 -0600

    NavBar style demo

--- lib/widgets/navbar_bottom.dart
+++ lib/widgets/navbar_bottom.dart
@@ -4,28 +4,40 @@ class NavbarBottom extends StatelessWidget {
   const NavbarBottom({super.key});
 
   final navbarStyle = const BoxDecoration(
-      color: Color.fromARGB(255, 225, 83, 135),
+      color: Color(0xFFFF7F50),
       borderRadius: BorderRadius.all(Radius.circular(20)));
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: const EdgeInsets.all(10),
         decoration: navbarStyle,
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             child: BottomNavigationBar(
-              backgroundColor: const Color(0xFFFF7F50),
+              unselectedItemColor: Colors.white,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
               items: const <BottomNavigationBarItem>[
                 BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
                   label: 'Home',
                 ),
                 BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_cart),
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
                   label: 'Cart',
                 ),
                 BottomNavigationBarItem(
-                  icon: Icon(Icons.account_box),
+                  icon: Icon(
+                    Icons.account_box,
+                    color: Colors.white,
+                  ),
                   label: 'Profile',
                 ),
               ],
