9b57a1b80e0e6da9af3105e7a216e1edca94ed9c
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-05 13:15:06 -0600

    Cahnged provider to another Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        // the '_'  (underscore) symbol is used as a parameter name to indicate that the parameter is not used or referenced in the function body (we can repalce it for the context parameter) on the provider function
-        providers: [
-          ChangeNotifierProvider(
-              create: (context) => FoodDataProvider()..loadFoodCards())
-        ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme().theme(),
-          title: 'Food App',
-          home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xFFFF7F50),
-              title: const Text('Welcome User', style: TextStyle()),
-              actions: const [
-                Padding(
-                    padding: EdgeInsets.only(right: 20),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://cdn0.iconfinder.com/data/icons/profile-lipo-glyph/64/profile_user_character-29-512.png'),
-                    )),
-              ],
-            ),
-            body: const HomePage(),
-            bottomNavigationBar: const NavbarBottom(),
-          ),
-        ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().theme(),
+      title: 'Food App',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFFF7F50),
+          title: const Text('Welcome User', style: TextStyle()),
+          actions: const [
+            Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://cdn0.iconfinder.com/data/icons/profile-lipo-glyph/64/profile_user_character-29-512.png'),
+                )),
+          ],
+        ),
+        body: MultiProvider(
+            // the '_'  (underscore) symbol is used as a parameter name to indicate that the parameter is not used or referenced in the function body (we can repalce it for the context parameter) on the provider function
+            providers: [
+              ChangeNotifierProvider(
+                  create: (context) => FoodDataProvider()..loadFoodCards())
+            ],
+            child: const HomePage()),
+        bottomNavigationBar: const NavbarBottom(),
+      ),
+    );
   }
 }
