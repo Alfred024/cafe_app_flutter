a31b755c85667b1c88119e7e6b92503199e9838a
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-12 08:46:31 -0600

    Profile page demo layout finished

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -5,6 +5,38 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Card(
+            color: const Color(0xFFFF7F50),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 2.0,
+            child: const SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Alfredo'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('21030761@itcelaya.edu.mx'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
